e4c6cda617b6f62a78faa161b590e34a89670056
Vasek-Doskar <doskar@spsjc.cz>
2025-09-24 11:53:28 +0200

    delegat1

--- ConsoleApp3/Program.cs
+++ ConsoleApp3/Program.cs
@@ -1,13 +1,48 @@
 ﻿internal class Program
 {
+
+    public delegate void Vypis(string text);
+
+    public static void Uloz(string text)
+    {
+        string cesta = "Soubor.txt";
+        using (StreamWriter sw = new StreamWriter(cesta, true))
+        {
+            sw.WriteLine(text);
+            sw.Flush();
+        }
+    }
+
+
+    static void VykonejFunkci(Vypis vypis, string text)
+    {
+        if (!string.IsNullOrEmpty(text) && vypis is not null)
+        {
+            vypis(text);
+        }
+    }
+
+
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
-        Console.WriteLine("Hello, World!");
-        Console.WriteLine("Hello, World!");
-        Console.WriteLine("Zadej slovo");
-        string slovo = Console.ReadLine();
-        Console.WriteLine($"Zadal jsi slovo: {slovo}");
-        // Vyliž si
+
+        //Delegát - zapouzdřený odkaz na nějakou funkci/metodu
+
+        Vypis konzole = Console.WriteLine;
+
+        konzole("Nazdárek Havlíku, co zuby?");
+
+        VykonejFunkci(Uloz, "Dávejte sakra pozor!!!");
+
+
+        VykonejFunkci(
+            (text) => Console.Write($"Zadaný text '{text}' má celkem {text.Length} znaků"), 
+            "Dávejte sakra pozor!!!"
+            );
+
+
+
+
+
     }
 }
