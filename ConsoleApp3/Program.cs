b15aa44eb8c678ee8d65a6fe31e7193e1b266502
Vasek-Doskar <doskar@spsjc.cz>
2025-09-24 11:01:35 +0200

    Duplication

--- ConsoleApp3/Program.cs
+++ ConsoleApp3/Program.cs
@@ -3,5 +3,14 @@
     private static void Main(string[] args)
     {
         Console.WriteLine("Hello, World!");
+        Console.WriteLine("Hello, World!");
+        Console.WriteLine("Hello, World!");
+        Console.WriteLine("Hello, World!");
+        Console.WriteLine("Hello, World!");
+        Console.WriteLine("Hello, World!");
+        Console.WriteLine("Hello, World!");
+        Console.WriteLine("Hello, World!");
+        Console.WriteLine("Hello, World!");
+        Console.WriteLine("Hello, World!");
     }
 }
