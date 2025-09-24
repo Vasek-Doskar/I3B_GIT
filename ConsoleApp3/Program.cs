eea849b8a2929ca47bfb1af4886d8d8b002c2a5a
Vasek-Doskar <doskar@spsjc.cz>
2025-09-24 12:06:05 +0200

    predpripravene delegaty

--- ConsoleApp3/Program.cs
+++ ConsoleApp3/Program.cs
@@ -34,15 +34,33 @@
 
         VykonejFunkci(Uloz, "Dávejte sakra pozor!!!");
 
+        Vypis LambdaFce = (text) => Console.Write($"Zadaný text '{text}' má celkem {text.Length} znaků");
+
 
         VykonejFunkci(
-            (text) => Console.Write($"Zadaný text '{text}' má celkem {text.Length} znaků"), 
+            delegate(string text) { Console.Write($"Zadaný text '{text}' má celkem {text.Length} znaků"); }, 
             "Dávejte sakra pozor!!!"
             );
 
+        //předpřipravené delegáty
 
+        //1. delegát typou void - akce - "public delegate void jmeno ();"
+        Action akce = () => Console.WriteLine("Hello Teeths");
+        akce();
 
+        Action<string> action = Console.WriteLine;
+        action("Čus Havlík, co zoubky?");
 
+        //2. delegát typu bool - predikát - "public delegate bool predikát (parametry)
+
+        Predicate<int> Sude = (num) => num % 2 == 0; //  True/False
+        Console.WriteLine(Sude(22)); // True
+
+        //3. delegát libovolného návratového typu funkce - Func - public delegate <typ> func (parametry)
+
+        Func<int, string> IntParser = (num) => num.ToString();
+        var value = IntParser(22);
+        Console.WriteLine($"proměnná value s hodnotou {value} je datového typu: {value.GetType().Name}");
 
     }
 }
